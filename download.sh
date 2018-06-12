25d52cf0aca7f6b261fa34e1fea228c66bd50172
Marc Rußwurm <marc.russwurm@tum.de>
2018-06-12 08:33:37 +0200

    updated download link

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget ftp://ftp.lrz.de/transfer/ijgi18/data_IJGI18_demo.zip
+wget https://s3.eu-central-1.amazonaws.com/corupublic/mtlcc/data_IJGI18_demo.zip
 unzip data_IJGI18_demo.zip
 rm data_IJGI18_demo.zip
